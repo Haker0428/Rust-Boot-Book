d66b220bf1b1afd534806198d0895ca8ffbe2307
Haker0428 <513193084@qq.com>
2023-12-11 22:22:53 +0800

    first bin run in qemu rasp pi4

--- first_bin/src/main.rs
+++ first_bin/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::arch::global_asm;
+use core::panic::PanicInfo;
+
+global_asm!(include_str!("boot.S"));
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub unsafe fn main(){
+    let _x = 1;
 }
